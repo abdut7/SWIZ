673518de60475b8392825f861743778deb74908f
Abdurahiman Thommangadan <abdurahiman.t@wins.local>
2021-11-13 22:29:54 +0530

    dfd

--- fe_client.js
+++ fe_client.js
@@ -1,20 +1,129 @@
-const express=require('express');
-const path=require('path')
-const app=express();
-try {
-app.use(express.static(path.join(__dirname,'trade-app-client/build')));
-app.use((req,res,next)=>{
-    res.sendFile(__dirname+'/trade-app-client/build/index.html')
-     next()
-})
-app.get('/*',(req,res)=>{
-    res.sendFile(__dirname+'/trade-app-client/build/index.html')
-})
-app.listen(4000, function () {
-    console.log('App is listening on port 4000' );
-});
-} catch (error) {
-    console.log(error);
-   // res.sendFile(__dirname,'trade-app-client','index.html')
-    
+server {
+    listen 80;
+    listen[::]: 80;
+
+    index index.html;
+    server_name swissbayedutech.live www.swissbayedutech.live;
+
+    location / {
+        proxy_pass http: //localhost:4000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+
+    }
 }
+
+server {
+
+    listen 80;
+    listen[::]: 80;
+
+
+    index index.html;
+
+    server_name admin.swissbayedutech.live www.admin.swissbayedutech.live;
+
+    location / {
+        proxy_pass http://localhost:4001;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+server {
+
+    listen 80;
+
+    listen[::]: 80;
+
+
+    index index.html;
+
+
+    server_name swissbayedutech.com www.swissbayedutech.com;
+    location / {
+
+        proxy_pass http://localhost:4000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+
+
+server {
+
+    listen 80;
+
+    listen[::]: 80;
+
+    index index.html;
+
+
+    server_name common.swissbayedutech.com www.common.swissbayedutech.com;
+    location / {
+
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+
+
+server {
+
+    listen 80;
+
+    listen[::]: 80;
+
+    index index.html;
+
+
+    server_name vedio.swissbayedutech.com www.vedio.swissbayedutech.com;
+    location / {
+
+        proxy_pass http://localhost:3001;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+
+
+server {
+
+    listen 80;
+
+    listen[::]: 80;
+
+
+    index index.html;
+
+
+    server_name trade.swissbayedutech.com www.trade.swissbayedutech.com;
+    location / {
+
+        proxy_pass http: //localhost:3002;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}
+
+sudo certbot --nginx -d swissbayedutech.live -d www.swissbayedutech.live -d admin.swissbayedutech.live -d www.admin.swissbayedutech.live  -d swissbayedutech.com -d www.swissbayedutech.com -d common.swissbayedutech.com -d www.common.swissbayedutech.com -d vedio.swissbayedutech.com -d www.vedio.swissbayedutech.com -d trade.swissbayedutech.com -d www.trade.swissbayedutech.com
+
+sudo certbot --nginx  -d www.admin.swissbayedutech.live   -d www.common.swissbayedutech.com  -d www.vedio.swissbayedutech.com -d www.trade.swissbayedutech.com
